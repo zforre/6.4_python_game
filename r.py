533c8036c19ebdf9845a8bf4b20e6be5d2a4ca52
RamonaEJSpence <ramonaejspence@gmail.com>
2020-02-13 11:02:17 -0500

    beginning of r.py

---
+++ r.py
@@ -0,0 +1,3 @@
+print('As you walk into this room, there is a floodlight pointed at the door, blinding you from the rest of the room.\n\
+It looks like there isn\'t an end. Use caution, I can\'t help in here')
+input('Select your action: Turn off the lamp? Walk around the Lamp?')
