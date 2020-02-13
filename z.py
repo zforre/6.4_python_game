40744061692ac1883e876b19ed1be868912924a4
Zach Forrest <ZachForrest@Zachs-MacBook-Pro.local>
2020-02-13 11:42:10 -0500

    starts to create room for use in game

---
+++ z.py
@@ -0,0 +1,11 @@
+print('You open the door farthest to the right, and take a step inside.')
+
+print('The room is empty, dark, and seems to be very small.')
+
+print('You notice a small table to the right of the entry, its too dark to see whats on it so you start to feel whats on top of it.')
+
+print("You feel a small metal object. It's a lighter, you pick it up try to ignite it. It doesn't work the first time, or the second, but finally on the third try it works!")
+
+print("The light illuminates only a small area around you, there seems to be scrathes all over the walls and floors around you")
+
+
